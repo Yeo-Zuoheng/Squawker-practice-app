8da76bca279293cf348e36bae858c1d530306e67
Yeo Zuoheng <yeo.zuoheng@gmail.com>
2017-09-15 15:54:58 +0800

    Sending to single device

---
+++ app/src/main/java/android/example/com/squawker/fcm/SquawkFirebaseInstanceIdService.java
@@ -0,0 +1,21 @@
+package android.example.com.squawker.fcm;
+
+
+import android.util.Log;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+public class SquawkFirebaseInstanceIdService extends FirebaseInstanceIdService{
+    private static String LOG_TAG  = SquawkFirebaseInstanceIdService.class.getSimpleName();
+    @Override
+    public void onTokenRefresh() {
+        String refreshedToken = FirebaseInstanceId.getInstance().getToken();
+        Log.d(LOG_TAG,"Refreshed Token:" + refreshedToken );
+        sendRegistrationToServer(refreshedToken);
+    }
+
+    private void sendRegistrationToServer(String token){
+
+    }
+}
